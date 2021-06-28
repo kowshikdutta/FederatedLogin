9f35bfd44195870744e19ae1ed38ac80aa6a3e17
Kowshik Dutta <kowshikdutta@yahoo.com>
2021-06-28 20:10:40 +0530

    Script to access Azure REST API

---
+++ CallingAzureAPI.ps1
@@ -0,0 +1,31 @@
+﻿$TenantId = ""
+$ClientId = ""
+$ClientSecret = ""
+
+$Resource = "https://management.core.windows.net/"
+$SubscriptionId = ""
+
+$RequestAccessTokenUri = "https://login.microsoftonline.com/$TenantId/oauth2/token"
+$body = "grant_type=client_credentials&client_id=$ClientId&client_secret=$ClientSecret&resource=$Resource"
+
+$Token = Invoke-RestMethod -Method Post -Uri $RequestAccessTokenUri -Body $body -ContentType 'application/x-www-form-urlencoded'
+
+Write-Host "Print Token" -ForegroundColor Green
+Write-Output $Token
+
+# Get the Azure Pricing API
+
+$pricingURI = 'https://prices.azure.com/api/retail/prices' 
+
+$Header = @{}
+
+$header.Add("Authorization","$($Token.token_type) "+ " " + "$($Token.access_token)")
+
+$Output = Invoke-RestMethod -Method Get -Uri $pricingURI -Headers $Header | ConvertTo-Json
+
+Write-Host $Output
+
+
+
+
+
